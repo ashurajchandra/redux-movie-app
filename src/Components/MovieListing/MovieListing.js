1c66df4ae9125f95cf6f3b648dd1e1f91c993af2
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-03-01 00:46:13 +0530

    added css to movie card comp

--- src/Components/MovieListing/MovieListing.js
+++ src/Components/MovieListing/MovieListing.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+
+import "./MovieListing.scss";
+import { MovieCard } from '..';
 import { getAllMovies } from '../../Features/Movies/MovieSlice';
 
 
 const MovieListing = () => {
     const movies = useSelector(getAllMovies);
     console.log("movies",movies);
+    let renderMovies ='';
+    renderMovies = movies.Response ==='True'?(
+        movies.Search.map((movie,index)=>(
+            <MovieCard key={index} data={movie}/>
+        ))
+    ):(<div>{movies.Error}</div>)
     return (
-        <div>
-            movie list
+        <div className='movie-wrapper'>
+            <div className='movie-list'>
+                <h2>Movies</h2>
+                <div className='movie-container'>{renderMovies}</div>
+            </div>
         </div>
     );
 };
